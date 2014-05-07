7ea1bb89e14c03da28295d049c084c81903ff58e
Steve Muckle <smuckle@codeaurora.org>
2014-05-06 18:05:50 -0700

    sched: add migration load change notifier for frequency guidance
    When a task moves between CPUs in two different frequency domains
    the cpufreq governor may wish to immediately modify the frequency
    of both the source and destination CPUs of the migrating task.
    
    A tunable is provided to establish what size task is considered
    "significant" enough to warrant notifying cpufreq.
    
    Also fix a bug that would cause load to not be accounted properly
    during wakeup migrations.
    
    Change-Id: Ie8f6b1cc4d43a602840dac18590b42a81327c95a
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2122,6 +2122,8 @@ extern unsigned int sysctl_sched_window_stats_policy;
 extern unsigned int sysctl_sched_init_task_load_pct;
 #endif
 
+extern unsigned int sysctl_sched_task_migrate_notify_pct;
+
 #ifdef CONFIG_SCHED_HMP
 extern unsigned int sysctl_sched_enable_hmp_task_placement;
 extern unsigned int sysctl_sched_mostly_idle_nr_run;
@@ -2157,6 +2159,9 @@ int sched_proc_update_handler(struct ctl_table *table, int write,
 		void __user *buffer, size_t *length,
 		loff_t *ppos);
 #endif
+
+extern int sched_migrate_notify_proc_handler(struct ctl_table *table,
+		int write, void __user *buffer, size_t *lenp, loff_t *ppos);
 
 extern int sched_hmp_proc_update_handler(struct ctl_table *table,
 		int write, void __user *buffer, size_t *lenp, loff_t *ppos);
@@ -2840,6 +2845,8 @@ struct migration_notify_data {
 	int dest_cpu;
 	int load;
 };
+
+extern struct atomic_notifier_head load_alert_notifier_head;
 
 extern long sched_setaffinity(pid_t pid, const struct cpumask *new_mask);
 extern long sched_getaffinity(pid_t pid, struct cpumask *mask);
