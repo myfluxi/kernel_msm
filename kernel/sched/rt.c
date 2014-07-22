2a5efe66e374815a7caad47e82f1caf4bbcb12b8
Steve Muckle <smuckle@codeaurora.org>
2014-07-22 15:12:32 -0700

    sched: fixes for compilation without CONFIG_SCHED_HMP
    These fixes are necessary to compile without CONFIG_SCHED_HMP
    enabled.
    
    Change-Id: Iabbde3c22a81288242ed3a44fdfdb2a16db8b072
    Signed-off-by: Steve Muckle <smuckle@codeaurora.org>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1498,6 +1498,7 @@ next_idx:
 
 static DEFINE_PER_CPU(cpumask_var_t, local_cpu_mask);
 
+#ifdef CONFIG_SCHED_HMP
 static int find_lowest_rq_hmp(struct task_struct *task)
 {
 	struct cpumask *lowest_mask = __get_cpu_var(local_cpu_mask);
@@ -1538,6 +1539,12 @@ static int find_lowest_rq_hmp(struct task_struct *task)
 	}
 	return best_cpu;
 }
+#else
+static int find_lowest_rq_hmp(struct task_struct *task)
+{
+	return -1;
+}
+#endif
 
 static int find_lowest_rq(struct task_struct *task)
 {
